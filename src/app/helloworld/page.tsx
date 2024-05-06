28c304d7287db1cd5715b1446e27ac218aaf26ea
Meet Rakholiya <meet@investifyd.com>
2024-05-06 11:48:50 +0530

    integrat shadcn ui and theme color

---
+++ src/app/helloworld/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className='text-4xl font-normal'> Hello World!! 😄🎉 </h1>
+        </main>
+    )
+}
+
+export default page
